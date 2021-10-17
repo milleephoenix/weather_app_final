e9e5118ec0e13dac54325a56ea8b5943c0caa426
milleephoenix <91555723+milleephoenix@users.noreply.github.com>
2021-10-17 19:29:26 -0400

    Set city in url

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 let apiKey = "339446a70a6285d4da506a17e7465ddf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Saguenay&appid=${apiKey}&units=metric`;
+let city = "Saguenay";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //console.log(apiUrl);
 
