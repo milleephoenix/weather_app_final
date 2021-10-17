4623bcdd0642b0425ede38e2e62f9859073391f6
milleephoenix <91555723+milleephoenix@users.noreply.github.com>
2021-10-17 15:11:30 -0400

    Create Display function and set ids

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 let apiKey = "339446a70a6285d4da506a17e7465ddf";
-let apiUrl = `https://.api.openweathermap.org/data/2.5/weather?q={city name}&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Saguenay&appid=${apiKey}&units=metric`;
+
+//console.log(apiUrl);
+
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+axios.get(apiUrl).then(displayTemperature);
